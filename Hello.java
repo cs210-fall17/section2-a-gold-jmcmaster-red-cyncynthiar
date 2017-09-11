5f6756029d14f280ce448831f5f83673e2f97e37
“crothschild” <cyncynthiar@email.arizona.edu>
2017-09-11 08:48:43 -0700

    red

--- Hello.java
+++ Hello.java
@@ -13,7 +13,7 @@ public class Hello {
         
         // Red Leader should add a print statement that says "Lock S-foils in attack position." 
        System.out.println("Lock S-foils in attack position.");
-
+       System.out.println("red is the best!!");
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
     }
