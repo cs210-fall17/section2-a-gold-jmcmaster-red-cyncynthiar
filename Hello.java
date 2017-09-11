4bc3c1a3fae3238f4464d3ad7d6477d74c125b55
Joey <jmcmaster@email.arizona.edu>
2017-09-11 08:52:48 -0700

    Failsafe

--- Hello.java
+++ Hello.java
@@ -12,7 +12,7 @@ public class Hello {
         
         // Red Leader should add a print statement that says "Lock S-foils in attack position." 
        System.out.println("Lock S-foils in attack position.");
-
+       System.out.println("red is the best!!");
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 		// Lets see if this comment
 		/*
